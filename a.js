315d48d4692bf7de312d0d863e3471089009edcc
munisekhar <munisekharudavalapati1@gmail.com>
2024-01-06 19:01:04 +0530

    Serving HTML , CSS and JS (VIEWS)

--- a.js
+++ a.js
@@ -45,6 +45,7 @@
 
 
 // const http = require('http');
+const path=require('path');
 const express=require('express');
 const bodyparser= require('body-parser')
 
@@ -59,6 +60,6 @@ app.use(adminroutes);
 app.use(shopeRouters)
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1> Page not found<h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','4040.html'));
 })
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,23 @@
 const express=require('express');
 
 const router=express.Router();
-
+   router.get('/login',(rq,res)=>{
+    res.send(`
+       <form action="/add-product" method="POST">
+       <input type="text" name="userid" >
+       <button type="submit">login</button>
+       </form>
+    `)
+   });
   //add-product=>get
-  router.get('/add-product',(req,res,next)=>{
+  router.post('/add-product',(req,res,next)=>{
     res.send('<form action="/prodects" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
   });
 
   //products=>Post
   router.post('/prodects',(req,res,next)=>{
-     console.log(req.body)
-      res.redirect('/');
+     console.log(req.body.title)
+      // res.redirect('/add-product');
   });
   
 
